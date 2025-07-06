983a8eceea290af73e1a2bdbd38c66ce15db8e54
Joshua Odedeyi <hollyray2003@gmail.com>
2025-07-06 19:24:17 +0100

    added task 5 correction to lowercase

--- task_5.sql
+++ task_5.sql
@@ -1,5 +1,5 @@
 -- File: task_5.sql
 -- script that inserts a single row in the table customer (database alx_book_store) in my MySQL server.
 
-INSERT INTO Customers (customer_id, customer_name, email, address)
+INSERT INTO customers (customer_id, customer_name, email, address)
 VALUES (1, 'Cole Baidoo', 'cbaidoo@sandtech.com', '123 Happiness Ave.');
